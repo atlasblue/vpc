31e5bc94b1b32d0581f7e8f81b603581b41d1edc
atlasblue <92083755+atlasblue@users.noreply.github.com>
2022-12-19 15:39:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,25 @@
 #################################################
+# NUTANIX PROVIDER DEFINITION
+#################################################
+
+terraform {
+  required_providers {
+    nutanix = {
+      source  = "nutanix/nutanix"
+      version = "1.8.0-beta.1"
+    }
+  }
+}
+
+provider "nutanix" {
+  username = var.NUTANIX_USERNAME
+  password = var.NUTANIX_PASSWORD
+  endpoint = var.NUTANIX_ENDPOINT
+  port     = var.NUTANIX_PORT
+  insecure = var.NUTANIX_INSECURE
+}
+
+#################################################
 # GET CLUSTERS DATA
 #################################################
 
